9a21d4cb4ad9b7f707c4b578099c85a1830eef0d
Up-Streamer <up_stream@hotmail.com>
2020-06-24 17:18:49 -0300

    Initial commit.

---
+++ nf_MfRc522/Constant.cs
@@ -0,0 +1,142 @@
+﻿// ReSharper disable UnusedMember.Global
+#pragma warning disable 1591
+
+namespace Driver.MfRc522
+{
+    namespace Constants
+    {
+        /// <summary>
+        /// Address of register
+        /// </summary>
+        public enum Register
+        {
+            // Dummy Register for tests
+            Test = 0x2b << 1,
+            // Command and status
+            Command = 0x01 << 1,
+            ComIrq = 0x04 << 1,
+            DivIrq = 0x05 << 1,
+            Error = 0x06 << 1,
+            Status1 = 0x07 << 1,
+            Status2 = 0x08 << 1,
+            FifoData = 0x09 << 1,
+            FifoLevel = 0x0A << 1,
+            Control = 0x0C << 1,
+            BitFraming = 0x0D << 1,
+            Coll = 0x0E << 1,
+
+            Mode = 0x11 << 1,
+            TxMode = 0x12 << 1,
+            RxMode = 0x13 << 1,
+            TxControl = 0x14 << 1,
+            TxAsk = 0x15 << 1,
+            Version = 0x37 << 1,
+
+            CrcResultHigh = 0x21 << 1,
+            CrcResultLow = 0x22 << 1,
+            ModeWith = 0x24 << 1,
+            TimerMode = 0x2A << 1,
+            TimerPrescaler = 0x2B << 1,
+            TimerReloadHigh = 0x2C << 1,
+            TimerReloadLow = 0x2D << 1,
+        }
+
+        /// <summary>
+        /// Return code of some functions
+        /// </summary>
+        public enum StatusCode
+        {
+            Ok,
+            Collision,
+            Error,
+            Timeout,
+            NoRoom,
+            CrcError
+        }
+
+        /// <summary>
+        /// Command to send to picc (card)
+        /// </summary>
+        public enum PiccCommand
+        {
+            ReqA = 0x26,
+            MifareRead = 0x30,
+            HaltA = 0x50,
+            AuthenticateKeyA = 0x60,
+            AuthenticateKeyB = 0x61,
+            SelCl1 = 0x93,
+            SelCl2 = 0x95,
+            SelCl3 = 0x97,
+        }
+
+        /// <summary>
+        /// Command of reader
+        /// </summary>
+        public enum PcdCommand
+        {
+            Idle = 0x00,
+            CalculateCrc = 0x03,
+            Transceive = 0x0c,
+            MfAuthenticate = 0xe,
+        }
+
+        /// <summary>
+        /// Length of uid
+        /// </summary>
+        public enum UidType
+        {
+            T4 = 4,
+            T7 = 7,
+            T10 = 10
+        }
+
+        /// <summary>
+        /// Type of card
+        /// </summary>
+        public enum PiccType
+        {
+            Unknown,
+            Mifare1K,
+            MifareUltralight
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public class Uid
+        {
+            /// <summary>
+            /// Lentgh of uid (can be 4, 7 or 10 bytes length)
+            /// </summary>
+            public UidType UidType { get; set; }
+
+            /// <summary>
+            /// Contain uid of card (can be 4, 7 or 10 bytes length)
+            /// </summary>
+            public byte[] UidBytes { get; set; }
+
+            /// <summary>
+            /// Sak which contains usefull informations
+            /// </summary>
+            public byte Sak { get; set; }
+
+            /// <summary>
+            /// Get type of card
+            /// </summary>
+            /// <returns>Type of card</returns>
+            public PiccType GetPiccType()
+            {
+                var sak = Sak & 0x7f;
+                switch (sak)
+                {
+                    case 0x08:
+                        return PiccType.Mifare1K;
+                    case 0x00:
+                        return PiccType.MifareUltralight;
+                    default:
+                        return PiccType.Unknown;
+                }
+            }
+        }
+    }
+}
---
+++ nf_MfRc522/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+//using Bauland.Others;
+using Driver.MfRc522;
+using Driver.MfRc522.Constants;
+using nanoFramework.Hardware.Esp32;
+
+
+
+// using Bauland.Others.Constants.MfRc522;
+//using GHIElectronics.TinyCLR.Devices.Gpio;
+//using GHIElectronics.TinyCLR.Pins;
+
+namespace testRC522
+{
+    static class Program
+    {
+        private static MfRc522 _mfRc522;
+        static void Main()
+        {
+            Setup();
+            
+            Debug.WriteLine("###############################################");
+            Debug.WriteLine("# SPI1");
+            Debug.WriteLine("###############################################");
+            Debug.WriteLine($"SPI1_CLOCK: " + Configuration.GetFunctionPin(DeviceFunction.SPI1_CLOCK));
+            Debug.WriteLine($"SPI1_MISO: " + Configuration.GetFunctionPin(DeviceFunction.SPI1_MISO));
+            Debug.WriteLine($"SPI1_MOSI: " + Configuration.GetFunctionPin(DeviceFunction.SPI1_MOSI));
+
+            Debug.WriteLine("");  
+            Debug.WriteLine($"Version: 0x{_mfRc522.GetVersion():X}");
+            Debug.WriteLine("");
+
+            //_mfRc522.Test();
+
+            InfiniteLoop();
+        }
+
+        private static void InfiniteLoop()
+        {
+            byte[] bufferAtqa = new byte[2];
+            byte[] defaultKey = { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff };
+
+            while (true)
+            {
+                //test
+               // bufferAtqa[0] = 0x0;
+                //bufferAtqa[1] = 0x0;
+                if (_mfRc522.IsNewCardPresent(bufferAtqa))
+                {
+                   Debug.WriteLine("Card detected...");
+                   Debug.WriteLine($"ATQA: 0x{bufferAtqa[1]:X2},0x{bufferAtqa[0]:X2}");
+
+                    var uid = _mfRc522.PiccReadCardSerial();
+                    if (uid != null)
+                    {
+                        DisplayUid(uid);
+                        try
+                        {
+                            byte pageOrSector = (byte)(uid.GetPiccType() == PiccType.Mifare1K ? 16 : 4);
+                            for (byte i = 0; i < pageOrSector; i++)
+                            {
+                               Debug.WriteLine($"{i}:");
+                                var buffer = _mfRc522.GetSector(uid, i, defaultKey /*, PiccCommand.AuthenticateKeyA*/);
+                                if (uid.GetPiccType() == PiccType.Mifare1K)
+                                {
+                                    var c = _mfRc522.GetAccessRights(buffer);
+                                    Display1kBuffer(buffer, c);
+                                }
+                                else if (uid.GetPiccType() == PiccType.MifareUltralight)
+                                {
+                                    DisplayUltralightBuffer(buffer);
+                                }
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                           Debug.WriteLine(ex.Message);
+                        }
+
+                        _mfRc522.Halt();
+                        _mfRc522.StopCrypto();
+                    }
+                }
+
+                Thread.Sleep(1000);
+            }
+            // ReSharper disable once FunctionNeverReturns
+        }
+
+        private static void DisplayUltralightBuffer(byte[][] buffer)
+        {
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                var line = "";
+                for (int j = 0; j < buffer[i].Length; j++)
+                {
+                    line += $"{buffer[i][j]:X2} ";
+                }
+               Debug.WriteLine(line);
+            }
+        }
+
+        private static void Display1kBuffer(byte[][] buffer, byte[] accessRights)
+        {
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                var line = "";
+                for (int j = 0; j < buffer[i].Length; j++)
+                {
+                    line += $"{buffer[i][j]:X2} ";
+                }
+                line += $"[{(accessRights[0] >> i) & 0x01} {(accessRights[1] >> i) & 0x01} {(accessRights[2] >> i) & 0x01}]";
+               Debug.WriteLine(line);
+            }
+        }
+
+        private static void DisplayUid(Uid uid)
+        {
+            string msg = "Uid of card is: ";
+            for (int i = 0; i < (int)uid.UidType; i++)
+            {
+                msg += $"{uid.UidBytes[i]:X2} ";
+            }
+            msg += $"SAK: {uid.Sak:X2}";
+           Debug.WriteLine(msg);
+            switch (uid.GetPiccType())
+            {
+                case PiccType.Mifare1K:
+                   Debug.WriteLine("PICC type: MIFARE 1K");
+                    break;
+                case PiccType.MifareUltralight:
+                   Debug.WriteLine("PICC type: MIFARE Ultralight");
+                    break;
+                default:
+                   Debug.WriteLine("PICC type: Unknown");
+                    break;
+            }
+        }
+        
+        private static void Setup()
+        {
+            _mfRc522 = new MfRc522("SPI1", 4, 5);
+            // _mfRc522 = new MfRc522(FEZ.SpiBus.Spi1, FEZ.GpioPin.D8, FEZ.GpioPin.D9);
+        }
+    }
+}
